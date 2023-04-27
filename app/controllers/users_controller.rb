3097d1051141787e503c57c52ac868ab079acfd7
ksng-potepan <min.fam462@gmail.com>
2023-04-27 23:10:26 +0900

    feat: Create Profile  Detail and Edit screens

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,4 +10,16 @@ class UsersController < ApplicationController
   def edit
     @user = current_user
   end
+
+  def update
+    @user = current_user
+    @user.update(username: params[:username], introduce: params[:introduce])
+    if @user.save
+      flash[:notice] = "更新しました"
+      redirect_to action: "profile"
+    else
+      flash.now[:alert] = "更新できませんでした"
+      render('users/edit')
+    end
+  end
 end
