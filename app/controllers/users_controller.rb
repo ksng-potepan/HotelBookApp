2f3d4e28e8afc4a6a159ce5e1a11130f942e4eec
ksng-potepan <min.fam462@gmail.com>
2023-04-27 22:15:04 +0900

    feat: Create Account  Detail and Edit screens

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < ApplicationController
+  def account
+    @user = current_user
+  end
+
+  def profile
+    @user = current_user
+  end
+
+  def edit
+    @user = current_user
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'users/account'
+  get 'users/profile'
+  get 'users/profile/edit' => 'users#edit'
+  post 'users/profile/update' => 'users#update'
+
   devise_for :users
   root to: "home#top"
 end
