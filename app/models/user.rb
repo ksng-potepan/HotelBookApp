e9fbd15093f0cea7b02c7204f4dcb1e6d201b842
ksng-potepan <min.fam462@gmail.com>
2023-04-30 16:35:09 +0900

    feat: Add Room Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
 
   validates :username, presence: true
   has_one_attached :image
+  has_many :rooms, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "home#top"
+
+  resources :rooms
 end
